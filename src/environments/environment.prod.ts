3ea07d164ceb554650316ed82c9180ef47e4c641
SoftwareEngineer01 <sistemsistemaweb@gmail.com>
2020-10-18 17:37:53 -0500

    Se agrega url para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 import { NgxLoggerLevel } from 'ngx-logger';
 
 export const environment = {
+  urlApi: 'https://music-concerts.herokuapp.com/api/',
   production: true,
   logLevel: NgxLoggerLevel.OFF,
   serverLogLevel: NgxLoggerLevel.ERROR
